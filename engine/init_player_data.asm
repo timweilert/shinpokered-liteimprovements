041f82da34a738a6fd33fac01df0622c474b71b2
timweilert <tim.weilert@gmail.com>
2022-08-02 22:57:57 -0600

    Finally figured out the initial money scheme and how commenting out the +1 in the wPlayerMoney line allowed me to shift the value to the 0th byte in the 3 money bytes, allowing for writing bigger amounts

--- engine/init_player_data.asm
+++ engine/init_player_data.asm
@@ -21,8 +21,23 @@ InitPlayerData2:
 	ld hl, wNumBoxItems
 	call InitializeEmptyList
 
-START_MONEY EQU $3000
-	ld hl, wPlayerMoney + 1
+START_MONEY EQU $0500
+    ; D347 D348 and D349 are the money addresses with D347 being the top portion of the 3 bytes. 
+	ld hl, wPlayerMoney ; + 1 ; commenting out the +1
+	; previously the code wrote $3000 to wPlayerMoney + 1, which is D347 + 1 == D348.
+	; what this did in effect was shift the amount written to the 2nd and 3rd bytes of the 3 wPlayerMoney bytes
+	; old way: 00 30 00, would give a beginning money of 3000
+	; new way: 05 00 00, gives 50,000 money.
+	; D347, MSB is 100,000's
+	; D347, LSB is 10,000's 
+	; D348, MSB is 1000's 
+	; D348, LSB is 100's 
+	; D349, MSB is 10's 
+	; D349, LSB is 1's 
+	; so in effect, the line ld hl, wPlayerMoney + X will determine which byte the code is addressing, 
+	; where X = 0 (1st byte), 1 (2nd byte), 2 (3rd byte)
+	; Since we're using the HL dark register we have 16 bits to work with, but this code could also be done 
+	; with a single byte if you didn't care about the finer level of detail. 
 	ld a, START_MONEY / $100
 	ld [hld], a
 	xor a
